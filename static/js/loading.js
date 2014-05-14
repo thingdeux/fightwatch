332eca87fbf25f955ca8eb2c84577fc42ae37c52
Joshua Johnson <me@josh.land>
2014-05-13 23:24:30 -0700

    Killed white space on html

--- static/js/loading.js
+++ static/js/loading.js
@@ -1,23 +1,12 @@
 $(document).ready(function(){
-
 	function checkDBUp() {
 		//If DB up, reload - else set a new timeout for 2 seconds
 		$.get("/checkDB", function (data, status) {
 			if (status == "success") {
-				if (data == "True") {					
-					setTimeout(function(){ checkDBUp() } , 3000);
-				}
-				else {
-					location.reload();
-				}
-
-			}
-			
-		});
-
-		//setTimeout(function()) { checkDBUp(), 3000	}		
+				if (data == "True") { setTimeout(function(){ checkDBUp() } , 3000);	}
+				else { location.reload(); }
+			}		
+		});		
 	}
-
 	setTimeout(function(){ checkDBUp() } , 3000);
-
 });
